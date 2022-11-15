e5c92d8f7933477779c3a00c1147226c10f80ca8
HongSLin <63325729+HongSLin@users.noreply.github.com>
2022-11-14 21:11:28 -0700

    Update home.php

--- view/home.php
+++ view/home.php
@@ -10,16 +10,49 @@ include('../view/header.php');
 ?>
 
 
-    <main>
-        <div class="welcome-wrapper" style="text-align:center">
-            <h1>E-commerce website coming soon </h1>
-        </div>
+<main>
+    <div class="welcome-wrapper" style="text-align:center">
+        <h1>E-commerce website coming soon </h1>
+    </div>
 
 
 
+    <!-- Display books with $all_books array -->
+    <div id="border">
+    <div id="books" class="row">
+        <?php for($x = 0; $x < sizeof($all_books); $x++) {?>
+            <figure>
+                
+                <!-- Display image with the image location -->
+                <img src="<?php echo $all_books[$x]['pictureName'] ;?>">
 
+                <!-- Dsiplay book name -->
+	            <figcaption><?php echo $all_books[$x]['bookName'] ;?></figcaption>
 
-    </main> 
+                <!-- Display price, if discount > 0 then display the price after discount -->
+                <span class="price">
+                    <?php if($all_books[$x]['discountPercent'] > 0.00) {
+
+                        //  Using s tag to cross out original price, and using number_format() to get it to display 2 decimal places
+                        echo "<s>$".$all_books[$x]['listPrice']."</s>&ensp;Now:&nbsp;$".number_format(($all_books[$x]['listPrice'] *(1.00 - $all_books[$x]['discountPercent'])), 2, '.', '');
+                    }
+                    else{
+                        echo "$".$all_books[$x]['listPrice'];
+                    }?>
+                </span>
+
+                <!-- The buy button -->
+                <a class="button" href="#">Buy</a>
+                    
+	        </figure>
+        
+        <!-- end for loop -->
+        <?php }?>
+    
+    <!-- end book div -->
+    </div><br><br><br>
+
+</main> 
 
 <?php include('../view/footer.php'); ?>
 
